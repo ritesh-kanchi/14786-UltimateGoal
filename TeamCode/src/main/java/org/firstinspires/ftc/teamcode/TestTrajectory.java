5b7324bc30566c68277970d5bc132f191484749b
Ritesh Kanchi <ritesh.kanchi@hotmail.com>
2021-01-23 10:38:36 -0600

    trajectories

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTrajectory.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTrajectory.java
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
@@ -14,11 +17,32 @@ public class TestTrajectory extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-        SampleMecanumDrive driveTrain = new SampleMecanumDrive(hardwareMap);
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
-        Trajectories trajectory = new Trajectories(driveTrain,this);
+        Trajectories trajectory = new Trajectories(drive,this);
 
-        waitForStart();
-        trajectory.forwardT(distance,0,0,0);
+        Pose2d startPose = new Pose2d(-60,50,Math.toRadians(90));
+
+        drive.setPoseEstimate(startPose);
+
+//        waitForStart();
+//        trajectory.forward(distance,0,0,0);
+
+        Trajectory traj1 = drive.trajectoryBuilder(startPose)
+//                .splineTo(new Vector2d(-45, 35),Math.toRadians(90))
+//                .lineTo(new Vector2d(-45,35))
+                .splineToConstantHeading(new Vector2d(-45, 35), Math.toRadians(90))
+                .build();
+
+        Trajectory traj2 = drive.trajectoryBuilder(traj1.end())
+//                .splineTo(new Vector2d(12, 61), Math.toRadians(90))
+//                .lineTo(new Vector2d(12,61))
+                .splineToConstantHeading(new Vector2d(12, 61), Math.toRadians(90))
+                .build();
+
+
+
+        drive.followTrajectory(traj1);
+        drive.followTrajectory(traj2);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Trajectories.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Trajectories.java
@@ -19,58 +19,72 @@ public class Trajectories {
     public void basePose() {
         Trajectory base = driveTrain.trajectoryBuilder(new Pose2d())
                 .build();
+        fTrajectory(base);
     }
 
     public void basePose(double cX, double cY, double cH) {
         Trajectory base = driveTrain.trajectoryBuilder(new Pose2d(cX,cY,cH))
                 .build();
+        fTrajectory(base);
     }
 
     public void forward(double distance) {
         Trajectory forward = driveTrain.trajectoryBuilder(new Pose2d())
                 .forward(distance)
                 .build();
+        fTrajectory(forward);
     }
 
     public void forward(double distance, double cX, double cY, double cH) {
         Trajectory forward = driveTrain.trajectoryBuilder(new Pose2d(cX,cY,cH))
                 .forward(distance)
                 .build();
+        fTrajectory(forward);
     }
 
     public void backward(double distance) {
         Trajectory backward = driveTrain.trajectoryBuilder(new Pose2d())
                 .back(distance)
                 .build();
+        fTrajectory(backward);
     }
 
     public void backward(double distance, double cX, double cY, double cH) {
-        Trajectory forward = driveTrain.trajectoryBuilder(new Pose2d(cX,cY,cH))
+        Trajectory backward = driveTrain.trajectoryBuilder(new Pose2d(cX,cY,cH))
                 .back(distance)
                 .build();
+        fTrajectory(backward);
     }
 
     public void strafeRight(double distance) {
         Trajectory right = driveTrain.trajectoryBuilder(new Pose2d())
                 .strafeRight(distance)
                 .build();
+        fTrajectory(right);
     }
 
     public void strafeRight(double distance, double cX, double cY, double cH) {
         Trajectory right = driveTrain.trajectoryBuilder(new Pose2d(cX,cY,cH))
                 .strafeRight(distance)
                 .build();
+        fTrajectory(right);
     }
 
     public void strafeLeft(double distance) {
         Trajectory left = driveTrain.trajectoryBuilder(new Pose2d())
                 .strafeLeft(distance)
                 .build();
+        fTrajectory(left);
     }
 
     public void strafeLeft(double distance, double cX, double cY, double cH) {
         Trajectory left = driveTrain.trajectoryBuilder(new Pose2d(cX,cY,cH))
                 .strafeLeft(distance)
                 .build();
+        fTrajectory(left);
+    }
+
+    private void fTrajectory(Trajectory traj) {
+        driveTrain.followTrajectory(traj);
     }
 }
