a728261f765996ac2aa98b0860dbf390ecabbfad
Ritesh Kanchi <ritesh.kanchi@hotmail.com>
2021-03-05 12:50:08 -0600

    general testing files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FastShooter.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Config
+@Autonomous(name = "Fast Shooter")
+public class FastShooter extends LinearOpMode {
+
+    private DcMotorEx shooterOne, shooterTwo;
+
+    public static double MOTOR_POWER = 1;
+
+    @Override
+    public void runOpMode() {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        shooterOne = hardwareMap.get(DcMotorEx.class, "shooterOne");
+        shooterTwo = hardwareMap.get(DcMotorEx.class, "leftEncoder");
+        
+
+        Pose2d startPose = new Pose2d(-48, -50, Math.toRadians(0));
+
+        drive.setPoseEstimate(startPose);
+
+        drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+
+                shooterOne.setPower(MOTOR_POWER);
+                shooterTwo.setPower(MOTOR_POWER);
+
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
@@ -0,0 +1,67 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Config
+@TeleOp(name = "ServoTest")
+public class ServoTest extends LinearOpMode {
+
+    public static double STARTING_POSITION = 0.2;
+    public static double ENDING_POSITION = 0;
+
+    private DcMotorEx shooterOne, shooterTwo;
+
+    public static double MOTOR_POWER = 1;
+
+
+    Servo pushServo,clawPosition;
+
+    @Override
+    public void runOpMode() {
+
+        pushServo = hardwareMap.get(Servo.class, "ringPush");
+//        clawServo = hardwareMap.get(Servo.class,"clawServo");
+
+        shooterOne = hardwareMap.get(DcMotorEx.class, "shooterOne");
+        shooterTwo = hardwareMap.get(DcMotorEx.class, "leftEncoder");
+
+//        intake = hardwareMap.get(DcMotorEx.class,"intakeMotor");
+
+        shooterOne.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        shooterTwo.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        pushServo.setPosition(STARTING_POSITION);
+        waitForStart();
+        while (opModeIsActive()) {
+            if (gamepad1.a) {
+                pushServo.setPosition(STARTING_POSITION);
+            }
+
+            if (gamepad1.b) {
+                pushServo.setPosition(ENDING_POSITION);
+            }
+
+            if (gamepad1.x) {
+                shooterOne.setPower(MOTOR_POWER);
+                shooterTwo.setPower(MOTOR_POWER);
+            }
+
+            if (gamepad1.y) {
+                shooterOne.setPower(0);
+                shooterTwo.setPower(0);
+            }
+
+//            if(gamepad1.dpad_left) clawServo.setPosition(0);
+//
+//            if(gamepad1.dpad_right) clawServo.setPosition(0.8);
+
+
+        }
+    }
+}
