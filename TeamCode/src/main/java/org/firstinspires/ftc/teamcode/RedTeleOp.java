0c567264a2588e8408682828aed3c588a5862788
Ritesh Kanchi <ritesh.kanchi@hotmail.com>
2021-05-08 01:12:47 -0500

    FINISHED AUTODRIVE

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedTeleOp.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.nextcore.NextTeleOp;
@@ -10,19 +13,29 @@ public class RedTeleOp extends NextTeleOp {
 
     private static Positions pos = new Positions();
 
+    Vector2d SHOOTING_POSITION = new Vector2d(pos.RED_SHOOTING_X, pos.RED_SHOOTING_Y);
+
+    Vector2d POWERSHOT_1 = new Vector2d(pos.RED_POWERSHOT_X, pos.RED_POWERSHOT_Y_1);
+    Vector2d POWERSHOT_2 = new Vector2d(pos.RED_POWERSHOT_X, pos.RED_POWERSHOT_Y_2);
+    Vector2d POWERSHOT_3 = new Vector2d(pos.RED_POWERSHOT_X, pos.RED_POWERSHOT_Y_3);
+
+
     public RedTeleOp() {
         super(pos.RED_ENDING_X,
-                pos.RED_ENDING_Y,
-                pos.RED_SHOOTING_X,
-                pos.RED_SHOOTING_Y);
+                pos.RED_ENDING_Y);
     }
 
     @Override
-    public void runOpMode() {
+    public void runOpMode() throws InterruptedException {
         super.runOpMode();
-        while (opModeIsActive()) {
-            super.runDrive();
-            super.runControls();
+        while (opModeIsActive() && !isStopRequested()) {
+
+            Pose2d poseEstimate = drive.getPoseEstimate();
+
+            super.driveData(poseEstimate);
+
+            super.autoControl(poseEstimate,SHOOTING_POSITION,POWERSHOT_1,POWERSHOT_2,POWERSHOT_3);
+
         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nextcore/NextTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nextcore/NextTeleOp.java
@@ -2,35 +2,43 @@ package org.firstinspires.ftc.teamcode.nextcore;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
+import org.firstinspires.ftc.teamcode.RedTeleOp;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
 
 @Config
 public class NextTeleOp extends LinearOpMode {
 
-    public double SHOOTING_X;
-    public double SHOOTING_Y;
-
     public double STARTING_X;
     public double STARTING_Y;
+
+    enum Mode {
+        DRIVER_CONTROL,
+        AUTOMATIC_CONTROL
+    }
+
+    Mode currentMode = Mode.DRIVER_CONTROL;
+
+    public static double TARGET_HEADING = Math.toRadians(0);
+
 
     public SampleMecanumDrive drive = null;
     public Mechanisms mech = null;
 
-    public NextTeleOp(double STARTING_X, double STARTING_Y, double SHOOTING_X, double SHOOTING_Y) {
+    public NextTeleOp(double STARTING_X, double STARTING_Y) {
         this.STARTING_X = STARTING_X;
         this.STARTING_Y = STARTING_Y;
-
-        this.SHOOTING_X = SHOOTING_X;
-        this.SHOOTING_Y = SHOOTING_Y;
     }
 
     @Override
-    public void runOpMode() {
+    public void runOpMode() throws InterruptedException {
         drive = new SampleMecanumDrive(hardwareMap);
         mech = new Mechanisms(hardwareMap);
 
@@ -65,22 +73,15 @@ public class NextTeleOp extends LinearOpMode {
 
         if (gamepad2.dpad_down) mech.wobbleArmControl(Mechanisms.wobbleArmPos.DOWN);
 
-        if (gamepad2.dpad_left) mech.wobbleControl(Mechanisms.wobblePos.OPEN);
-
-        if (gamepad2.dpad_right) mech.wobbleControl(Mechanisms.wobblePos.CLOSE);
-
-        if (gamepad2.b) mech.wobbleTurretControl(Mechanisms.wobbleTurretPos.IN);
-
-        if (gamepad2.a) mech.wobbleTurretControl(Mechanisms.wobbleTurretPos.OUT);
 
         // idk what this is
-        if (gamepad2.y) {
+        if (gamepad2.dpad_left) {
             mech.stickOne.setPosition(1);
             telemetry.addLine("eeeeeee");
             telemetry.addLine();
         }
 
-        if (gamepad2.x) {
+        if (gamepad2.dpad_right) {
             mech.stickOne.setPosition(0.5);
             telemetry.addLine("aaaaa");
             telemetry.addLine();
@@ -88,8 +89,6 @@ public class NextTeleOp extends LinearOpMode {
     }
 
     public void runDrive() {
-        Pose2d poseEstimate = drive.getPoseEstimate();
-
         // Mecanum Drive Control
         drive.setWeightedDrivePower(
                 new Pose2d(
@@ -99,7 +98,13 @@ public class NextTeleOp extends LinearOpMode {
                 )
         );
 
+
+
+    }
+
+    public void driveData(Pose2d poseEstimate) {
         drive.update();
+
 
         telemetry.addData("x", poseEstimate.getX());
         telemetry.addData("y", poseEstimate.getY());
@@ -110,7 +115,53 @@ public class NextTeleOp extends LinearOpMode {
         telemetry.update();
     }
 
-    public void autoDrive() {
+    public void autoControl(Pose2d poseEstimate, Vector2d SHOOTING_POSITION,Vector2d POWERSHOT_1,Vector2d POWERSHOT_2,Vector2d POWERSHOT_3) {
+        switch (currentMode) {
+            case DRIVER_CONTROL:
+                runDrive();
+                runControls();
 
+                if (gamepad2.a) {
+                    Trajectory toShoot = drive.trajectoryBuilder(poseEstimate)
+                            .splineTo(SHOOTING_POSITION, TARGET_HEADING)
+                            .build();
+                    drive.followTrajectory(toShoot);
+
+                    currentMode = Mode.AUTOMATIC_CONTROL;
+                } else if (gamepad2.x) {
+                    Trajectory toLeftShot = drive.trajectoryBuilder(poseEstimate)
+                            .splineTo(POWERSHOT_1, TARGET_HEADING)
+                            .build();
+                    drive.followTrajectory(toLeftShot);
+
+                    currentMode = Mode.AUTOMATIC_CONTROL;
+                } else if (gamepad2.y) {
+                    Trajectory toMiddleShot = drive.trajectoryBuilder(poseEstimate)
+                            .splineTo(POWERSHOT_2, TARGET_HEADING)
+                            .build();
+                    drive.followTrajectory(toMiddleShot);
+
+                    currentMode = Mode.AUTOMATIC_CONTROL;
+                } else if (gamepad2.b) {
+                    Trajectory toRightShot = drive.trajectoryBuilder(poseEstimate)
+                            .splineTo(POWERSHOT_3, TARGET_HEADING)
+                            .build();
+                    drive.followTrajectory(toRightShot);
+
+                    currentMode = Mode.AUTOMATIC_CONTROL;
+                }
+                break;
+            case AUTOMATIC_CONTROL:
+                if (gamepad2.left_trigger > 0) {
+                    drive.cancelFollowing();
+                    currentMode = Mode.DRIVER_CONTROL;
+                }
+
+                if (!drive.isBusy()) {
+                    currentMode = Mode.DRIVER_CONTROL;
+                }
+                break;
+
+        }
     }
 }
